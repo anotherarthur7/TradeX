8b7eb32c29143378dea583460e3b42c0aa8b6772
anotherarthur7 <arturgaynanov7@gmail.com>
2025-02-21 11:11:17 +0700

    Added Offer test class, separate pages for them

--- Djenv/tradeapp/urls.py
+++ Djenv/tradeapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path("", views.home, name="home"),
-    path("offers", views.items, name="offers")
+    #path("offers", views.items, name="offers"),
+    path("<int:req_id>", views.itemIndex, name="itemIndex")
 ]
--- Djenv/tradeapp/views.py
+++ Djenv/tradeapp/views.py
@@ -6,6 +6,9 @@ from .models import Offer
 def home(request):
 	return render(request, "home.html")
 
-def items(request):
-	offers = Offer.objects.all() # retrieves ALL info about DB's Item object
-	return render(request, "offers.html", {"offers" : offers})
+#def items(request):
+	#offers = Offer.objects.all() # retrieves ALL info about DB's Item object
+	#return render(request, "offers.html", {"offers" : offers})
+
+def itemIndex(request, req_id):
+	return render(request, "offers.html", {"offer" : Offer.objects.get(id=req_id)}) 
