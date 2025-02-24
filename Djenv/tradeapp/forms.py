243a788a8bb4772920e1108093176da1ec796b94
anotherarthur7 <arturgaynanov7@gmail.com>
2025-02-24 13:07:47 +0700

    Customized html a bit

--- Djenv/tradeapp/forms.py
+++ Djenv/tradeapp/forms.py
@@ -3,10 +3,25 @@ from django.forms import ModelForm
 from .models import Offer
 
 class OfferForm(ModelForm):
-    title = forms.TextInput()
-    description = forms.TextInput()
-    price = forms.DecimalField(max_digits=10, decimal_places=2)
-    image = forms.ImageField()
+    title = forms.CharField(
+        label="Offer Title",
+        widget=forms.TextInput(attrs={'class': 'custom-form-control', 'placeholder': 'Enter title'})
+    )
+    description = forms.CharField(
+        label="Description",
+        widget=forms.Textarea(attrs={'class': 'custom-form-control', 'placeholder': 'Enter description', 'rows': 4})
+    )
+    price = forms.DecimalField(
+        label="Price",
+        max_digits=10,
+        decimal_places=2,
+        widget=forms.NumberInput(attrs={'class': 'custom-form-control', 'placeholder': 'Enter price'})
+    )
+    image = forms.ImageField(
+        label="Image",
+        widget=forms.FileInput(attrs={'class': 'custom-form-control'})
+    )
+
     class Meta:
         model = Offer
         fields = ['title', 'description', 'price', 'image']
