cbf93892dd8715a071aca1fc387b028ae1f5fb46
anotherarthur7 <arturgaynanov7@gmail.com>
2025-03-16 15:06:21 +0700

    ajax implemented

--- Djenv/register/views.py
+++ Djenv/register/views.py
@@ -3,14 +3,11 @@ from register.forms import RegisterForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.http import JsonResponse
 
 # Create your views here.
 def register(response):
     
-    storage = messages.get_messages(response)
-    for message in storage:
-        pass 
-
     if response.user.is_authenticated:
         return redirect('home')
     
@@ -18,13 +15,15 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-            messages.success(response, 'Registration successful! Please log in.')
-            return redirect('login')
+            return JsonResponse({'success': True})
+        else:
+            # Return form errors as JSON
+            errors = {field: form.errors[field] for field in form.errors}
+            return JsonResponse({'success': False, 'errors': errors})
     else:
         form = RegisterForm()
         
-        
-    return render(response, "register.html", {"form":form})
+    return render(response, "register.html", {"form": form})
 
 
     
